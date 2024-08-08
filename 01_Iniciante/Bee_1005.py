9322b45e19f378379d6bdad3b6d7a75136fe0c9a
Matheus <matheusparreira.cruz@gmail.com>
2024-08-08 17:34:50 -0300

    Novo

---
+++ 01_Iniciante/Bee_1005.py
@@ -0,0 +1,15 @@
+'''
+beecrowd | 1005
+Média 1
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+
+https://judge.beecrowd.com/pt/problems/view/1005
+'''
+
+A = float(input())
+B = float(input())
+
+
+
